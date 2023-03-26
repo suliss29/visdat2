27354a6763f495d8184c2766a34353f3f8ba40d5
suliss29 <128945440+suliss29@users.noreply.github.com>
2023-03-26 14:02:28 +0700

    Update visdat2_app.py

--- visdat2_app.py
+++ visdat2_app.py
@@ -8,7 +8,7 @@ import pandas as pd
 with st.expander("Eksplorasi Data"):
     st.snow()
     st.title('Configure Plot')
-    st.header('Pilih dan lakukan eksplorasi')
+    st.header('Pilih data')
     option_x = st.selectbox(
         '',
         (' gdppercap ', '  Area ', ' birthrate ', '   Current account balance  ', '  Death rate ', '  Electricity consumption ', '   Electricity production  ', '   Exports  ', '   GDP  ', ' Country ', '  GDP real growth rate ', '   Highways  ', '   Imports  ', '  Industrial production growth rate ', '  Infant mortality rate ', '  Inflation rate  ', '   Internet users  ', '  Investment ', '   Labor force  ', ' lifeexpectatbirth ', '  Military expenditures ', '   Natural gas consumption  ', '   Oil consumption  ', ' population ', '  Public debt ', '  Railways ', '   Reserves of foreign exchange & gold  ', '  Total fertility rate ', '  Unemployment rate '))
