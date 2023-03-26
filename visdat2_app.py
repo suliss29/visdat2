3d418eeb3d23bb98663ecb750e14a513bd87d500
suliss29 <128945440+suliss29@users.noreply.github.com>
2023-03-26 11:32:51 +0700

    Update visdat2_app.py

--- visdat2_app.py
+++ visdat2_app.py
@@ -1,22 +1,45 @@
+import plotly.express as px
 import streamlit as st
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import pandas as pd
-import matplotlib.pyplot as plt
 
-# membaca file csv
-data = pd.read_csv('factbook.csv')
+with st.sidebar:
+    st.title('Configure Plot')
+    st.header('Silahkan Eksplorasi')
+    option_x = st.selectbox(
+        '',
+        (' gdppercap ', '  Area ', ' birthrate ', '   Current account balance  ', '  Death rate ', '  Electricity consumption ', '   Electricity production  ', '   Exports  ', '   GDP  ', ' Country ', '  GDP real growth rate ', '   Highways  ', '   Imports  ', '  Industrial production growth rate ', '  Infant mortality rate ', '  Inflation rate  ', '   Internet users  ', '  Investment ', '   Labor force  ', ' lifeexpectatbirth ', '  Military expenditures ', '   Natural gas consumption  ', '   Oil consumption  ', ' population ', '  Public debt ', '  Railways ', '   Reserves of foreign exchange & gold  ', '  Total fertility rate ', '  Unemployment rate '))
+    st.write('X :', option_x)
 
-# menampilkan judul aplikasi
-st.title('Visualisasi Data')
+    option_y = st.selectbox(
+        '',
+        (' lifeexpectatbirth ', '  Area ', ' birthrate ', '   Current account balance  ', '  Death rate ', '  Electricity consumption ', '   Electricity production  ', '   Exports  ', '   GDP  ', ' gdppercap ', '  GDP real growth rate ', '   Highways  ', '   Imports  ', '  Industrial production growth rate ', '  Infant mortality rate ', '  Inflation rate  ', '   Internet users  ', '  Investment ', '   Labor force  ', ' Country ', '  Military expenditures ', '   Natural gas consumption  ', '   Oil consumption  ', ' population ', '  Public debt ', '  Railways ', '   Reserves of foreign exchange & gold  ', '  Total fertility rate ', '  Unemployment rate '))
+    st.write('Y :', option_y)
 
-# menampilkan dataframe
-st.write(data)
+    option_size = st.selectbox(
+        '',
+        (' population ', '  Area ', ' birthrate ', '   Current account balance  ', '  Death rate ', '  Electricity consumption ', '   Electricity production  ', '   Exports  ', '   GDP  ', ' gdppercap ', '  GDP real growth rate ', '   Highways  ', '   Imports  ', '  Industrial production growth rate ', '  Infant mortality rate ', '  Inflation rate  ', '   Internet users  ', '  Investment ', '   Labor force  ', ' lifeexpectatbirth ', '  Military expenditures ', '   Natural gas consumption  ', '   Oil consumption  ', ' Country ', '  Public debt ', '  Railways ', '   Reserves of foreign exchange & gold  ', '  Total fertility rate ', '  Unemployment rate '))
+    st.write('Size :', option_size)
 
-# membuat histogram
-st.subheader('Histogram')
-plt.hist(data['Area'], bins=range(min(data['Birth rate']), max(data['Death rate']) + 1, 10))
-st.pyplot()
+    option_color = st.selectbox(
+        '',
+        (' birthrate ', '  Area ', ' Country ', '   Current account balance  ', '  Death rate ', '  Electricity consumption ', '   Electricity production  ', '   Exports  ', '   GDP  ', ' gdppercap ', '  GDP real growth rate ', '   Highways  ', '   Imports  ', '  Industrial production growth rate ', '  Infant mortality rate ', '  Inflation rate  ', '   Internet users  ', '  Investment ', '   Labor force  ', ' lifeexpectatbirth ', '  Military expenditures ', '   Natural gas consumption  ', '   Oil consumption  ', ' population ', '  Public debt ', '  Railways ', '   Reserves of foreign exchange & gold  ', '  Total fertility rate ', '  Unemployment rate '))
+    st.write('Color :', option_color)
 
-# membuat scatter plot
-st.subheader('Scatter Plot')
-plt.scatter(data['Area'], data['Birth rate'])
-st.pyplot()
+bub = st.slider('Ukuran Bubble', 1, 100, 60)
+df = pd.read_csv('factbook.csv')
+fig1 = px.scatter(df,
+    x=option_x,
+    y=option_y,
+    size=option_size,
+    color=option_color,
+    size_max=bub,
+    hover_data=[option_size]
+)
+fig2 = make_subplots(rows=1, cols=1)
+fig2.add_trace(go.Scatter(x=df[option_x],y=df[option_y], mode='markers', marker=dict(size=10, color='green')))
+fig2.add_trace(go.Scatter(x=df[option_x],y=df[option_y], mode='lines', line=dict(width=0.3, color='red'), showlegend=False))
+
+st.plotly_chart(fig1)
+st.plotly_chart(fig2)
